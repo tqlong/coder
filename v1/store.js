bf72e001cb25ad91c7df6b363060b3169c56ee3d
tqlong <tqlong>
2017-06-22 11:09:53 +0700

    setup knex

--- v1/store.js
+++ v1/store.js
@@ -1,6 +1,8 @@
+const knex = require('knex')(require('./knexfile'))
+
 module.exports = {
     createUser({username, password}) {
         console.log(`Adding user ${username} with password ${password}`)
-        return Promise.resolve()
+        return knex('user').insert({username, password})
     }
 }
