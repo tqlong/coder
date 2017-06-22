3cc75a85feffca2ec3a36c7290bb218353111048
tqlong <tqlong>
2017-06-22 12:07:58 +0700

    login route

--- v1/index.js
+++ v1/index.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
 const store = require('./store')
-
 const app = express()
+
 app.use(express.static('public'))
 app.use(bodyParser.json())
 
@@ -12,6 +11,16 @@ app.post('/createUser', (req, res) => {
         username: req.body.username,
         password: req.body.password
     }).then(() => res.sendStatus(200))
+})
+
+app.post('/login', (req, res) => {
+    store.authenticate({
+        username: req.body.username,
+        password: req.body.password
+    }).then(({success}) => {
+        if (success) res.sendStatus(200)
+        else res.sendStatus(401)
+    })
 })
 
 app.listen(7555, () => {
--- v1/store.js
+++ v1/store.js
@@ -5,8 +5,7 @@ function randomString() {
     return crypto.randomBytes(4).toString('hex')
 }
 
-function saltHashPassword(password) {
-    const salt = randomString()
+function saltHashPassword({password, salt = randomString()}) {
     const hash = crypto.createHmac('sha512', salt).update(password)
 
     return {
@@ -17,12 +16,25 @@ function saltHashPassword(password) {
 
 module.exports = {
     saltHashPassword,
+    
     createUser({username, password}) {
         console.log(`Adding user ${username} with password ${password}`)
-        const {salt, hash} = saltHashPassword(password)
+        const {salt, hash} = saltHashPassword({password})
         return knex('user').insert({
             username, 
             salt,
             encrypted_password: hash})
-    }
+    },
+
+    authenticate({username, password}) {
+        console.log(`Authenticate user ${username} with password ${password}`)
+
+        return knex('user').where({username})
+        .then(([user]) => {
+            if (!user) return {success: false}
+            const {hash} = saltHashPassword({password, salt: user.salt})
+            return {success: hash === user.encrypted_password}
+        })
+    },
+
 }
